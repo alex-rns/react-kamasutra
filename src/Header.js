b6a8ddca3cb4caaf8b32d6a3e143ba8481c8d7f9
Alex-rns <alexrns3@gmail.com>
2019-10-14 00:07:27 +0300

    10

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <a href="">1</a>
+            <a href="">2</a>
+            <a href="">3</a>
+        </div>
+    )
+};
+
+export default Header;
