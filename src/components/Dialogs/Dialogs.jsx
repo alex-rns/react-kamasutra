9493d089bb7e09c64fc4abbdcf69a0b6aef8fcfe
Alex-rns <alexrns3@gmail.com>
2019-10-14 22:48:48 +0300

    21. Start dev Dialog page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Alex
+                </div>
+                <div className={s.dialog}>
+                    Anna
+                </div>
+                <div className={s.dialog}>
+                    Lena
+                </div>
+                <div className={s.dialog}>
+                    Petr
+                </div>
+            </div>
+            <div className="messages">
+                <div className="message">Hi</div>
+                <div className="message">Hello</div>
+                <div className="message">Boo!</div>
+            </div>
         </div>
     )
 };
